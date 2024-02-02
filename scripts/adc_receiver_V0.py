f30863ec932394975301f7cac5bb8bd1265a67eb
Cedric Vioui <cedric.viou@obs-nancay.fr>
2024-02-02 14:45:55 +0100

    Add NCO test

--- scripts/adc_receiver_V0.py
+++ scripts/adc_receiver_V0.py
@@ -112,7 +112,6 @@ class NRT_channelizer(object):
                    'select_4f64_chan2_status',
                    'select_4f64_chan3_status',
                    'sync_cnt',
-                   'channelizer_ovr',
                    'armed_sync_cnt',
                    #'TenGbE0_data_overflow',
                    #'TenGbE0_tx_afull',
@@ -318,12 +317,203 @@ for receiver, config in zip(mydesign.Receivers, receiver_configs):
   receiver.network_config(config["dst_IP"], config["dsp_port"])
 
 
+# check that NCO tables are programmed properly
+if True:
+  for receiver, config in zip(mydesign.Receivers, receiver_configs):
+    prev_tables = receiver.NCO_tables.copy()
+    receiver.NCO_read_tables()
+    new_tables = receiver.NCO_tables.copy()
+    assert (prev_tables == new_tables).all()
 
 
 
 print('Wait for half second and arm PPS_trigger')
 mydesign.arm_PPS()
 mydesign.monitor()
+
+#mydesign.input_sel(source='ADC')
+#mydesign.conf_arb_gen(mode="constant", amp=10)
+
+
+# get and plot some data from mixer and dec_fir to chek data formats and overflows
+for receiver in mydesign.Receivers:
+  snap_names = ("L0_0_din0_in",
+                "L0_0_mult_out",
+                "L0_0_cast_out",
+                #"dec_fir0_sum_out",
+                #"dec_fir0_sr5_out",
+                "dec_out",
+                )
+  raw_fmt = False                
+  snap_data = {}
+  fig, axs = plt.subplots(figsize=(10, 10),
+                          nrows = len(snap_names), 
+                          ncols = 1,
+                          sharex='col',
+                          #sharey='col',
+                          )
+  if len(snap_names) == 1:
+    axs = (axs,)
+  for ax, snap_name in zip(axs, snap_names):
+    (d_w, d_dp), dat = receiver.get_snapshot(snap_name=snap_name, raw_fmt=raw_fmt)
+    integer_part_w = d_w - d_dp - 1
+    max_modulus = 2**integer_part_w
+    if len(dat.shape) == 1:
+      dat = dat[:, None]
+    mean = np.mean(dat, axis=0)
+    std = np.std(dat, axis=0)
+    ax.plot(dat)
+    snap_data[snap_name] = dat
+    title = snap_name + "\n$\mu \pm \sigma =$"
+    for m,s in zip(mean, std):
+      title +=  "\n$%4.2f \pm %4.2f$" % (m, s)
+    title = title[:-1] + "$"
+    ax.set_title(title)
+    ax.set_ylim((-max_modulus, max_modulus))
+    
+    if snap_name == "dec_out":
+      dat.shape = (-1, 2, 2)
+      dat = dat[..., 0] + 1j * dat[..., 1]
+      DAT = np.fft.fft(dat, axis=0)
+      DAT = np.fft.fftshift(DAT, axes=0)
+      DAT = 10*np.log10(DAT.real**2 + DAT.imag**2)
+      fig_tx, ax = plt.subplots(figsize=(10, 10))
+      ax.plot(DAT)
+      ax.axvline(1023, c='r')
+      ax.axvline(2048+1024, c='r')
+plt.show(block=False)
+
+
+Nacc = 100
+for receiver in mydesign.Receivers:
+  snap_name = "dec_out"
+  dats = []
+  t0 = datetime.datetime.utcnow()
+  for itt in tqdm.tqdm(range(Nacc)):
+    (d_w, d_dp), dat = receiver.get_snapshot(snap_name=snap_name, raw_fmt=raw_fmt)
+    dats.append(dat)
+  t1 = datetime.datetime.utcnow()
+
+  t0 = matplotlib.dates.date2num(t0)
+  t1 = matplotlib.dates.date2num(t1)
+
+  dats =  np.array(dats)
+  Nacc, Nfft, _ = dats.shape
+  dats.shape = (Nacc, Nfft, 2, 2)
+  dats = dats[..., 0] + 1j * dats[..., 1]
+  DATs = np.fft.fft(dats, axis=1)
+  DATs = np.fft.fftshift(DATs, axes=1)
+  DATs = DATs.real**2 + DATs.imag**2
+  DAT_min = DATs.min(axis=0)
+  DAT_mean = DATs.mean(axis=0)
+  DAT_max = DATs.max(axis=0)
+  df = np.arange(-Nfft/2, Nfft/2, dtype=float)/Nfft * (receiver.Fe/receiver.NCO_par)
+  f = (receiver.Fc + df)
+  ymin, ymax = -40, 40
+
+  fig, axs = plt.subplots(nrows=2, ncols=2,
+                          sharex='all', sharey='row',
+                          figsize=(10, 10))
+
+  spectral_lines = {"HI": (1420405751.768,),
+                    "OH": (1612.231e6, 1665.402e6, 1667.359e6, 1720.530e6,),
+                    "CH": (3263.794e6, 3335.481e6, 3349.193e6,),
+                    }
+  def plot_spectral_lines(ax):
+    for idx, (name, freqs) in enumerate(spectral_lines.items()):
+      for line in freqs:
+          ax.axvline(line, c='b')
+
+  plot_spectral_lines(axs[0, 0])
+  plot_spectral_lines(axs[0, 1])
+
+  axs[0, 0].fill_between(f, 10*np.log10(DAT_min[:,0].T), 10*np.log10(DAT_max[:,0].T) , facecolor='r', alpha=0.5)
+  axs[0, 1].fill_between(f, 10*np.log10(DAT_min[:,1].T), 10*np.log10(DAT_max[:,1].T) , facecolor='r', alpha=0.5)
+  axs[0, 0].plot(f, 10*np.log10(DAT_mean[:,0].T),'k')
+  axs[0, 1].plot(f, 10*np.log10(DAT_mean[:,1].T),'k')
+  axs[0, 1].set_ylim((ymin, ymax))
+
+  med = 10*np.log10(np.median(DATs[:,Nfft/2,:].flatten()))
+  mad = 10*np.log10(np.median(np.abs(DATs[:,Nfft/2,:]-med).flatten()))
+
+  axs[1, 0].imshow(10*np.log10(DATs[:,:,0]),
+                   extent=(f[0], f[-1], t0, t1),
+                   aspect='auto',
+                   vmin=med-3*mad, vmax=med+3*mad,
+                   origin='upper',
+                   cmap='gray',
+                   interpolation='nearest'
+                   )
+  axs[1, 1].imshow(10*np.log10(DATs[:,:,1]),
+                   extent=(f[0], f[-1], t0, t1),
+                   aspect='auto',
+                   vmin=med-3*mad, vmax=med+3*mad,
+                   origin='upper',
+                   cmap='gray',
+                   interpolation='nearest'
+                   )
+  axs[1, 0].yaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M:%S'))                 
+  axs[1, 1].yaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M:%S'))                 
+  axs[1, 0].yaxis_date()
+  axs[1, 1].yaxis_date()
+  ticks = (f[0], f[Nfft/4], f[Nfft/2],  f[3*Nfft/4], f[-1])
+  for (name, lines) in spectral_lines.items():
+    for line in lines:
+      if f[0] < line < f[-1]:
+        ticks = ticks + (line, )
+  ticks = list(ticks)
+  ticks.sort()
+  axs[1, 1].set_xticks(ticks)
+  axs[1, 0].set_xlabel('Freq (Hz)')
+  axs[1, 1].set_xlabel('Freq (Hz)')
+  plt.tight_layout()
+
+plt.show(block=False)
+
+
+
+
+for receiver in mydesign.Receivers:
+  snap_names = ("ADC0L0_snap0_3",
+                "ADC0L0_snap4_7",
+                "ADC0L0_snap8_11",
+                "ADC0L0_snap12_15",
+                )
+  # arm snaps
+  mydesign.fpga.write_int("receiver0_ADC0L0_snap_trig", 0)
+  for snap_name in snap_names:
+    snap = mydesign.fpga.snapshots[receiver.receiver_basename + snap_name]
+    snap.arm(man_valid=True)
+  # trig all snaps
+  mydesign.fpga.write_int("receiver0_ADC0L0_snap_trig", 1)
+  # read back content
+  data = []
+  for snap_name in snap_names:
+    snap = mydesign.fpga.snapshots[receiver.receiver_basename + snap_name]
+    data.append(snap.read_raw(read_nowait=True))
+  dt, shape, d_w, d_dp = np.dtype('int16'), (-1, 4, 2), 16, 15  # 4 parallele samples, complex
+  dt = dt.newbyteorder('>')
+  data = [np.frombuffer(d[0]['data'], dtype=dt).copy() for d in data]
+  data = [d.reshape(shape) for d in data]
+  data = np.concatenate(data, axis=-2)
+  data.shape = (-1, 2)
+  fig, ax = plt.subplots(nrows=2)
+  ax[0].plot(data[:,0], '.', label='Re')
+  ax[0].plot(data[:,1], '.', label='Im')
+  ax[0].legend()
+  data = data / 2.0**d_dp
+  data = data[:,0] + 1j*data[:,1]
+  NFFT = 2048
+  data.shape = (-1, NFFT)
+  DATA = abs(np.fft.fft(data, axis=-1))
+  ax[1].plot(np.arange(NFFT, dtype=float)/NFFT, 20*np.log10(DATA.mean(axis=0)), '.')
+  ax[1].set_xlim((0,1))
+
+plt.show(block=False)
+
+
+
+
 
 print('Started!!!')
 time.sleep(1)
